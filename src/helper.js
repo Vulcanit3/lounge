87ede88a69de7614df0c4a0cfa7f6fe3ed54644a
Tea23 <joe@warhaggis.com>
2017-08-15 19:13:14 +0000

    Remove upstream versioning

--- src/helper.js
+++ src/helper.js
@@ -48,7 +48,7 @@ function getGitCommit() {
 	}
 	try {
 		_gitCommit = require("child_process")
-			.execSync("git rev-parse --short HEAD 2> /dev/null") // Returns hash of current commit
+			.execSync("git rev-parse --short HEAD> /dev/null") // Returns hash of current commit
 			.toString()
 			.trim();
 		return _gitCommit;
